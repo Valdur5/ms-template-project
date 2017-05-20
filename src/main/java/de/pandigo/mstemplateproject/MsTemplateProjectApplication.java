b977272cfe39bebc62ae9d5efcda3ddd17e13e38
Valentin Durst <ValentinDurst@gmail.com>
2017-05-20 14:48:51 +0200

    Initial commit

---
+++ src/main/java/de/pandigo/mstemplateproject/MsTemplateProjectApplication.java
@@ -0,0 +1,12 @@
+package de.pandigo.mstemplateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTemplateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTemplateProjectApplication.class, args);
+	}
+}
