c16bb06817ff56dc1e311320f6b259994646fa23
Valentin Durst <ValentinDurst@gmail.com>
2017-05-20 19:13:40 +0200

    Cleaned up dependencies and moved the main file into the ms-ws module because the tutorial from spring.io suggested it. And because I had problems to build the deployment jar. With the main class now moved it seems to work.

--- ms-ws/src/main/java/de/pandigo/MsTemplateProjectApplication.java
+++ ms-ws/src/main/java/de/pandigo/MsTemplateProjectApplication.java
@@ -1,4 +1,4 @@
-package de.pandigo.mstemplateproject;
+package de.pandigo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
